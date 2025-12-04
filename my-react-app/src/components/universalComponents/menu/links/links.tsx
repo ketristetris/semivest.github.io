5e45b2104fb3a6c09e8ce4f5d52f7d8d37c7c331
ketristetris <ketristetris5@gmail.com>
2025-12-04 03:29:25 +0300

    link problem

--- my-react-app/src/components/universalComponents/menu/links/links.tsx
+++ my-react-app/src/components/universalComponents/menu/links/links.tsx
@@ -1,7 +1,18 @@
 
+const Link = ((logo), {linkName}) => {
+
+    return(
+        <div className="single-link">
+            <img src={logo} alt={linkName}></img>
+            <div className="link-txt">{linkName}</div>
+        </div>
+    )
+}
+
 const Links = () => {
     return(
         <div >
+            <Link/>
         </div>
     )
 }
--- my-react-app/src/components/universalComponents/menu/logo/logo.tsx
+++ my-react-app/src/components/universalComponents/menu/logo/logo.tsx
@@ -1,10 +1,14 @@
 import logo from "./../../../../images/logo.png"
+import React from "react"
 
-
-const Logo = () => {
+const Logo: React.FC = () => {
     return(
         <div className="logo">
-            <img src=logo alt="" />
+            <img src={logo} alt="Semivest Logo" />
+            <div className="logoName">
+                <div className="companyName">Semivest</div>
+                <div className="accountVersion">Demo account</div>
+            </div>
         </div>
     )
 }
